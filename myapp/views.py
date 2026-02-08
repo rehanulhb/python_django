56cd875372226692fcf73aee9768a3a28d6c3640
Rehanul hasan Bappy <rehanulh@gmail.com>
2026-02-08 02:13:29 +0100

    Added Routes and Views

---
+++ myapp/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+
+
+def home(request):
+    #Process 1
+    #Process 2
+    #Process 3
+    return HttpResponse("Hello APP")
+
+
+def profile(request):
+    # Process 1
+    # Process 2
+    # Process 3
+    return HttpResponse("Hello Profile")
+
+def dashboard(request):
+    # Process 1
+    # Process 2
+    # Process 3
+    return HttpResponse("Hello dashboard")
+
+
